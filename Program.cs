57eeedf86cdab757a8dee76e9141d82c8ee20d27
RomanPrinkovskij <117101302+RomanPrinkovskij@users.noreply.github.com>
2022-11-21 19:41:23 +0200

    Hw6_1-2

--- Program.cs
+++ Program.cs
@@ -1,48 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CW5
+namespace Homework_06_tast_06
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            int cont = 0;
-           Dictionary<int,string> dic = new Dictionary<int,string>();
-            for (int i = 0; i < 7; i++)
+        start:
+            try
             {
-                Console.Write("Enter id of person = ");
-                int id = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter Name of person - ");
-                string name = Console.ReadLine();
-                dic.Add(id, name);
+                Console.WriteLine("Enter int one -");
+                double one = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter int two -");
+                double two = Convert.ToDouble(Console.ReadLine());
+                if (two == 0) { throw  new DivideByZeroException(); }
+                Console.WriteLine("{0}/{1}={0} ",one, two, Div(one, two));
+                if (one < two) { throw new ApplicationException(" one must be >b"); }
+                Console.WriteLine("{0}/{1}={0} ", one, two, Div(one, two));
             }
-            Console.Write("Enter the id of person that wanted - ");
-            int find = Convert.ToInt32(Console.ReadLine());
-            foreach (KeyValuePair<int, string> kvp in dic)
+            catch (DivideByZeroException rx)
             {
-                
-                if (find == kvp.Key)
-                {
-                    
-                    Console.WriteLine("The Name of Person whis this id is -  {0}", kvp.Value);
-                }
-                else
-                {
-                    cont++;
-                }
+                Console.WriteLine(rx.Message);
+                goto start;
+               
             }
-            if (cont==7)
+            catch (FormatException x)
             {
-                Console.WriteLine("We haven`t person with this id:{0}",find);
+                Console.WriteLine(x.Message);
+                goto start;
+
             }
+            catch (ApplicationException ap)
+            {
+                Console.WriteLine(ap.Message);
+                goto start;
 
-
-
+            }
+            finally
+            {
+                Console.WriteLine("The End");
+                Console.WriteLine("________");
+            }
         }
+        private static double Div(double a, double b)
+        {
+            return a / b;
+        }
+
     }
 }
