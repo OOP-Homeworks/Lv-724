ce6535cd658d5c7fa743fe3b6ac5c79840f40ad2
Dima <58148797+Dima-212@users.noreply.github.com>
2022-11-28 16:40:23 +0200

    HM5.Dima

--- Program.cs
+++ Program.cs
@@ -1,95 +1,35 @@
 ﻿using System;
-using System.Reflection.Emit;
-using System.Threading;
+using System.Collections.Generic;
 
-namespace DudnykHW5
+namespace HomeWork5._2.Dima
 {
-    internal class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Task 1, 2
-            int firstVal, secondVal;
-
-            Begining:
-            try 
-            { 
-                Console.Write("Enter first number: ");
-                firstVal = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter second number: ");
-                secondVal = Convert.ToInt32(Console.ReadLine());
-                if (secondVal == 0) 
-                    throw new DivideByZeroException();
-            }
-            catch (DivideByZeroException ex)
+            Dictionary<uint, string> catalogue = new Dictionary<uint, string>();
+            catalogue.Add(11, "Julia");
+            catalogue.Add(12, "Ivan");
+            catalogue.Add(13, "Dima");
+            catalogue.Add(14, "Petro");
+            catalogue.Add(15, "Ivanka");
+            catalogue.Add(16, "Zorana");
+            catalogue.Add(17, "Veronika");
+            Console.WriteLine("Enter the ID");
+            string numbers = Console.ReadLine();
+            uint number = Convert.ToUInt32(numbers);
+            foreach (char c in catalogue.Keys)
             {
-                Console.WriteLine(ex.Message);
-                goto Begining;
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-                goto Begining;
-            }
-
-
-            Console.WriteLine(Div(firstVal, secondVal));
-
-
-        // Task 3
-            label2: 
-            int start, end;
-            Console.Write("Enter start number: ");
-            start = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter end number: ");
-            end = Convert.ToInt32(Console.ReadLine());
-
-            try
-            {
-                int[] array = ReadNumber(start, end);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                goto label2;
-            }
-
-            Console.WriteLine("All good");
-          
-        }
-
-        static double Div(int firstVal, int secondVal)
-        {
-            return (double) firstVal / secondVal;
-        }
-
-        static int[] ReadNumber (int start, int end)
-        {
-            int arrSize = end - start;
-            if (arrSize < 10)
-                throw new Exception ("There is no room for 10 integer numbers to be in range");
-
-            int[] array = new int [arrSize];
-
-            for (int i = 0; i < arrSize; i++)
-            {                
-                Console.Write($"Enter the {i} integer: ");
-                array[i] = Convert.ToInt32(Console.ReadLine());
-
-                if (array[i] < start || array[i] > end)
+                if (c == number)
                 {
-                    throw new Exception("Out of bounds, try again");
+                    Console.WriteLine($"The name is {catalogue[c]}");
                 }
-
-                if ((i > 0) && (array[i] < array[i - 1]))
+                else if (c != number)
                 {
-                    throw new Exception("This number is smaller than previous, must be bigger");
+                    Console.WriteLine("No name with such ID");
                 }
-
+                //Я зробив, що на всі інші ID пише "No name with such ID", а щоб просто виводилось, якщо нема такого ID не додумався
             }
-
-            return array;
-            
         }
     }
 }
