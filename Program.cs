750a1e0e97476ad485860d67ae0f3b631c003335
amoreCaro <117117217+amoreCaro@users.noreply.github.com>
2022-12-12 15:51:02 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,166 +1,56 @@
-
-﻿using System;
-
-
-
-namespace CalculatorSpace
+﻿namespace Homework_8
 {
-    public class Calculator
-    {
-        public int Addition (int x, int y)
-        {
-            return x + y;
-        }
-
-        public int Subtraction (int x, int y)
-        {
-            return x - y;
-        }
-
-        public int Multiplication(int x, int y)
-        {
-            return x * y;
-        }
-
-        public float Division(int x, int y)
-        {
-            return (float)x / (float)y;
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Triangle triangle1 = new Triangle(new Point(1, 2), new Point(5, 5), new Point(5, 2));
-            Triangle triangle2 = new Triangle(new Point(3, 2), new Point(5, 4), new Point(8, 2));
-            Triangle triangle3 = new Triangle(new Point(3, 4), new Point(0, 0), new Point(2, 2));
-            Triangle triangle4 = new Triangle(new Point(1, 8), new Point(8, 3), new Point(4, 5));
-            Triangle triangle5 = new Triangle(new Point(3, 6), new Point(3, 5), new Point(5, 0));
+            var listOfShapes = new List<Shape>();
 
-            List < Triangle > triangles = new List<Triangle>();
-            triangles.Add(triangle1);
-            triangles.Add(triangle2);
-            triangles.Add(triangle3);
-            triangles.Add(triangle4);
-            triangles.Add(triangle5);
-
-            var closestToOrigin = triangles.OrderBy(l => l.MinDistanceToOrigin()).FirstOrDefault();
-
-            
-
-            Console.WriteLine("Perimeter is: {0}", triangle1.Perimeter());
-            Console.WriteLine(closestToOrigin);
-
-            Console.ReadKey();
-            
-            bool control;
-            do
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Give me two int numb:");
-                Console.WriteLine("First number:");
-                var userInput = int.Parse(Console.ReadLine());
-                Console.WriteLine("Second number:");
-                var userInput2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Which figure do you choose? \n1.Circle or \n2.Square?");
+                var userInput = Console.ReadKey().KeyChar;
+                if (userInput == '1')
+                {
+                    Console.WriteLine("Give me a length of the radius");
+                    var length = Console.ReadLine();
+                    Console.WriteLine("Give me a name");
+                    var name = Console.ReadLine();
+                    var shape = new Circle(name, int.Parse(length));
+                    listOfShapes.Add(shape);
 
-                var numbers = new Hw_Math();
-                try
-                {
-                    var result = numbers.Div(userInput, userInput2);
-                    control = false;
                 }
-                catch (DivideByZeroException ex)
+                else
                 {
-                    Console.WriteLine("Cant be divided by 0");
-                    Console.WriteLine(ex);
-                    Console.WriteLine("Try again");
-                    control = true;
+                    Console.WriteLine("Give me a length of the side");
+                    var length = Console.ReadLine();
+                    Console.WriteLine("Give me a name");
+                    var name = Console.ReadLine();
+                    var shape = new Square(name, int.Parse(length));
+                    listOfShapes.Add(shape);
+
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("Something went wrong, try again");
-                    throw;
-                }
-            } while (control);
+            }
+
+            foreach (var item in listOfShapes)
+            {
+                Console.WriteLine($"{item.Name}");
+                item.Area();
+                item.Perimeter();
+            }
+
+            var biggest = listOfShapes.OrderByDescending(e => e.Perimeter1)
+                .First();
+            Console.WriteLine($"{biggest.Name} is the biggest {biggest.Perimeter1}");
+
+            listOfShapes = listOfShapes.OrderBy(e => e.Area1)
+                .ToList();
+            foreach (var item in listOfShapes)
+            {
+                Console.WriteLine($"Sorted by area : {item.Area1}");
+            }
+            Console.ReadKey();
 
         }
     }
-
-    public struct Point
-    {
-        public int x;
-        public int y;
-
-        public Point (int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public override string ToString()
-        {
-            return $"x: {x}, y: {y}\n";
-        }
-    }
-
-    public class Triangle
-    {
-        public Point coordinate1;
-        public Point coordinate2;
-        public Point coordinate3;
-
-        public Triangle(Point coordinate1, Point coordinate2, Point coordinate3)
-        {
-            this.coordinate1 = coordinate1;
-            this.coordinate2 = coordinate2;
-            this.coordinate3 = coordinate3;
-        }
-
-        public double DistanceBetween1and2()
-        {
-            return Math.Sqrt(Math.Pow(coordinate1.x - coordinate2.x, 2) + Math.Pow(coordinate1.y - coordinate2.y, 2));
-        }
-
-        public double DistanceBetween1and3()
-        {
-            return Math.Sqrt(Math.Pow(coordinate1.x - coordinate3.x, 2) + Math.Pow(coordinate1.y - coordinate3.y, 2));
-
-        }
-
-        public double DistanceBetween2and3()
-        {
-            return Math.Sqrt(Math.Pow(coordinate2.x - coordinate3.x, 2) + Math.Pow(coordinate2.y - coordinate3.y, 2));
-
-        }
-
-        public double Perimeter()
-        {
-            return DistanceBetween1and2() + DistanceBetween1and3() + DistanceBetween2and3();
-        }
-
-        public double MinDistanceToOrigin()
-        {
-            List<double> distancesToOrigin = new List<double>();
-            distancesToOrigin.Add(Math.Sqrt(Math.Pow(0 - coordinate1.x, 2) + Math.Pow(0 - coordinate1.y, 2)));
-            distancesToOrigin.Add(Math.Sqrt(Math.Pow(0 - coordinate2.x, 2) + Math.Pow(0 - coordinate2.y, 2)));
-            distancesToOrigin.Add(Math.Sqrt(Math.Pow(0 - coordinate3.x, 2) + Math.Pow(0 - coordinate3.y, 2)));
-
-            var minDistance = distancesToOrigin.OrderBy(l => l).FirstOrDefault();
-            return minDistance;
-        }
-
-
-        public override string ToString()
-        {
-            return $"{coordinate1.ToString()}{coordinate2.ToString()}{coordinate3.ToString()}";
-        }
-
-    }
-
 }
-
