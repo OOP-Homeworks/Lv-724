ab06f27809c16ea723123e3b8bc286c3f3b6b266
√42 <102050884+sqrt42@users.noreply.github.com>
2022-12-15 01:11:54 +0200

    hw8 seletskiy

---
+++ Seletskiy_HW8/Shape.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW8
+{
+    public abstract class Shape : IComparable<Shape>
+    {
+        private string name;
+
+        public string Name {
+            get {
+                return name; 
+            } 
+        }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public virtual int CompareTo(Shape secondShape)
+        {
+            Shape shapeOne = secondShape as Shape;
+            if (shapeOne == null)
+            {
+                throw new ArgumentException("That's not a shape");
+            }
+            else
+            {
+                return this.Area().CompareTo(secondShape.Area());
+            }
+        }
+    }
+}
