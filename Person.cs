df58e43186822aced2713eda19bc434b2d875595
StanislavCherniak <117373172+StanislavCherniak@users.noreply.github.com>
2022-12-15 11:26:17 +0200

    Add files via upload

--- Person.cs
+++ Person.cs
@@ -1,46 +1,58 @@
 ﻿using System;
-namespace Polymorphism
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4HW_Cherniak
 {
-    public abstract class Person : IComparable
-    {
-        protected string fullName;
-
-        public virtual string Name
-        {
-            get
-            {
-                return fullName;
-            }
-            set
-            {
-                fullName = value;
-            }
-        }
-        public Person(string name)
-        {
-            this.fullName = name;
-        }
-
-        public virtual void Display()
-        {
-            Console.WriteLine("Name: {0}", fullName);
-        }
-
-        public int CompareTo(object obj)
-        {
-            if (obj != null)
-            {
-                Person p = obj as Person;
-                if (p != null)
-                {
-                    return this.fullName.CompareTo(p.fullName);
-                }
-                else
-                    throw new ArgumentException("The object is not a \"Person\"");
-            }
-            else
-                return 1;
-        }
-    }
+	public class Person
+	{
+		private string name;
+		private DateTime birthYear;
+		public string Name
+		{
+			get { return name; }	
+		}
+		public DateTime BirthYear
+		{
+			get { return birthYear; }	
+		}
+		public Person(string name, DateTime birthYear)
+		{
+			this.name = name;
+			this.birthYear = birthYear;
+		}
+		public int Age()
+		{
+			int age = (DateTime.Today.Year) - (birthYear.Year);
+			return age;	
+		}
+		public static Person Input(int i)
+		{
+			Console.WriteLine($"Please, input name {i + 1} person :  ");
+			string name = Console.ReadLine();
+			Console.WriteLine($"Please, input the year of birth('days'. 'month'. 'year'): ");
+			var birthYear = Convert.ToDateTime(Console.ReadLine());	
+			Person person = new Person(name, birthYear);
+			return person;
+		}
+		public void ChangeName(int age)
+		{
+			if (age < 16)
+			{
+				name = "Very Young";	
+			}
+		}
+		public void Output()
+		{
+			Console.WriteLine($"{name} was born {birthYear}");
+		}
+		public static bool operator ==(Person first, Person second)
+		{
+			return (first.name == second.name);
+		}
+		public static bool operator !=(Person first, Person second)
+		{
+			return !(first.name == second.name);	
+		}
+	}
 }
-
--- Program.cs
+++ Program.cs
@@ -1,166 +1,41 @@
-
 ﻿using System;
 
-
-
-namespace CalculatorSpace
+namespace _4HW_Cherniak
 {
-    public class Calculator
-    {
-        public int Addition (int x, int y)
-        {
-            return x + y;
-        }
-
-        public int Subtraction (int x, int y)
-        {
-            return x - y;
-        }
-
-        public int Multiplication(int x, int y)
-        {
-            return x * y;
-        }
-
-        public float Division(int x, int y)
-        {
-            return (float)x / (float)y;
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Triangle triangle1 = new Triangle(new Point(1, 2), new Point(5, 5), new Point(5, 2));
-            Triangle triangle2 = new Triangle(new Point(3, 2), new Point(5, 4), new Point(8, 2));
-            Triangle triangle3 = new Triangle(new Point(3, 4), new Point(0, 0), new Point(2, 2));
-            Triangle triangle4 = new Triangle(new Point(1, 8), new Point(8, 3), new Point(4, 5));
-            Triangle triangle5 = new Triangle(new Point(3, 6), new Point(3, 5), new Point(5, 0));
-
-            List < Triangle > triangles = new List<Triangle>();
-            triangles.Add(triangle1);
-            triangles.Add(triangle2);
-            triangles.Add(triangle3);
-            triangles.Add(triangle4);
-            triangles.Add(triangle5);
-
-            var closestToOrigin = triangles.OrderBy(l => l.MinDistanceToOrigin()).FirstOrDefault();
-
-            
-
-            Console.WriteLine("Perimeter is: {0}", triangle1.Perimeter());
-            Console.WriteLine(closestToOrigin);
-
-            Console.ReadKey();
-            
-            bool control;
-            do
-            {
-                Console.WriteLine("Give me two int numb:");
-                Console.WriteLine("First number:");
-                var userInput = int.Parse(Console.ReadLine());
-                Console.WriteLine("Second number:");
-                var userInput2 = int.Parse(Console.ReadLine());
-
-                var numbers = new Hw_Math();
-                try
-                {
-                    var result = numbers.Div(userInput, userInput2);
-                    control = false;
-                }
-                catch (DivideByZeroException ex)
-                {
-                    Console.WriteLine("Cant be divided by 0");
-                    Console.WriteLine(ex);
-                    Console.WriteLine("Try again");
-                    control = true;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Something went wrong, try again");
-                    throw;
-                }
-            } while (control);
-
-        }
-    }
-
-    public struct Point
-    {
-        public int x;
-        public int y;
-
-        public Point (int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public override string ToString()
-        {
-            return $"x: {x}, y: {y}\n";
-        }
-    }
-
-    public class Triangle
-    {
-        public Point coordinate1;
-        public Point coordinate2;
-        public Point coordinate3;
-
-        public Triangle(Point coordinate1, Point coordinate2, Point coordinate3)
-        {
-            this.coordinate1 = coordinate1;
-            this.coordinate2 = coordinate2;
-            this.coordinate3 = coordinate3;
-        }
-
-        public double DistanceBetween1and2()
-        {
-            return Math.Sqrt(Math.Pow(coordinate1.x - coordinate2.x, 2) + Math.Pow(coordinate1.y - coordinate2.y, 2));
-        }
-
-        public double DistanceBetween1and3()
-        {
-            return Math.Sqrt(Math.Pow(coordinate1.x - coordinate3.x, 2) + Math.Pow(coordinate1.y - coordinate3.y, 2));
-
-        }
-
-        public double DistanceBetween2and3()
-        {
-            return Math.Sqrt(Math.Pow(coordinate2.x - coordinate3.x, 2) + Math.Pow(coordinate2.y - coordinate3.y, 2));
-
-        }
-
-        public double Perimeter()
-        {
-            return DistanceBetween1and2() + DistanceBetween1and3() + DistanceBetween2and3();
-        }
-
-        public double MinDistanceToOrigin()
-        {
-            List<double> distancesToOrigin = new List<double>();
-            distancesToOrigin.Add(Math.Sqrt(Math.Pow(0 - coordinate1.x, 2) + Math.Pow(0 - coordinate1.y, 2)));
-            distancesToOrigin.Add(Math.Sqrt(Math.Pow(0 - coordinate2.x, 2) + Math.Pow(0 - coordinate2.y, 2)));
-            distancesToOrigin.Add(Math.Sqrt(Math.Pow(0 - coordinate3.x, 2) + Math.Pow(0 - coordinate3.y, 2)));
-
-            var minDistance = distancesToOrigin.OrderBy(l => l).FirstOrDefault();
-            return minDistance;
-        }
-
-
-        public override string ToString()
-        {
-            return $"{coordinate1.ToString()}{coordinate2.ToString()}{coordinate3.ToString()}";
-        }
-
-    }
-
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var persons = new Person[6];
+			for (int i = 0; i < persons.Length; i++)
+			{
+				persons[i] = Person.Input(i);
+			}
+			for(int i = 0; i < persons.Length; i++)
+			{
+				var age = persons[i].Age();
+				Console.WriteLine($"{persons[i].Name} is {age} years old");	
+			}
+			for (int i = 0; i < persons.Length; i++)
+			{
+				int age = persons[i].Age();
+				persons[i].ChangeName(age);
+			}
+			for (int i = 0; i < persons.Length; i++)
+			{
+				persons[i].Output();
+			}
+			for (int i = 0; i < persons.Length; i++)
+			{
+				for	(int j = i + 1; j < persons.Length; j++)
+				{
+					if (persons[i] == persons[j])
+					{
+						Console.WriteLine($"{persons[i].Name} and {persons[j].Name} are equals");
+					}
+				}
+			}
+			Console.ReadKey();
+		}
+	}
 }
-
