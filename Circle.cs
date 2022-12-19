47d7cb69037c779b30f4f5a3ce25b690bf888d9e
amoreCaro <117117217+amoreCaro@users.noreply.github.com>
2022-12-19 18:55:50 +0200

    Add files via upload

--- Circle.cs
+++ Circle.cs
@@ -2,24 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-
-namespace Sytnyk_Illia_s_Homework_8
+namespace HW9
 {
     public class Circle : Shape
     {
-        double radius;
+        private double radius;
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+        }
+
         public Circle(string name, double radius) : base(name)
         {
             this.radius = radius;
         }
-        public double Radius { get { return radius; } }
+
         public override double Area()
         {
             return Math.PI * Math.Pow(radius, 2);
         }
+
         public override double Perimeter()
         {
-            return 2 * Math.PI * radius;
+            return Math.PI * 2 * radius;
         }
     }
-    
+}
